d9f71bb66f9622e92bd23329874bf80b036253cc
Baranitharan Chittharanjan <baranitharanece@gmail.com>
2024-06-30 16:09:42 +1200

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -63,7 +63,7 @@ func main() {
 	var enableHTTP2 bool
 	var clusterResourceNamespace string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8082", "The address the probe endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&clusterResourceNamespace, "cluster-resource-namespace", "", "the namespace for external alert secret")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
@@ -110,7 +110,7 @@ func main() {
 	}
 
 	webhookServer := webhook.NewServer(webhook.Options{
-		TLSOpts: tlsOpts,
+		CertDir: "/",
 	})
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
